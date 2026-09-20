49e50c4f30a13a9edce39db49c46569126e82a30
mayron073 <yesidcaseres484@gmail.cmo>
2026-09-20 18:26:01 -0500

    Corrige configuracion de GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Cambia este valor si el repositorio tiene otro nombre.
-  base: '/https://github.com/mayron073/MR.git/'
+  base: '/MR/'
 });
